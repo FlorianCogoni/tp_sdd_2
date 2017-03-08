2ef928f62176cb5fb7efda5b77e85e0576d03232
olibouv <oli.bouv@yahoo.fr>
2017-03-08 21:52:43 +0100

    Update truc.c

--- truc.c
+++ truc.c
@@ -23,17 +23,49 @@ void truc_rec (int i, int n, int T[]){
     }
 }
 
-/*void truc_iter (int i, int n, int T[]){
+void truc_iter(int i, int n, int T[])
+{
+    int j,k;
+    int *e;
+    pile_t * p=initpile(TAILLE);
     int fin = 0;
-    init
-    while(!fin){
 
+    while (!fin)
+    {
+        while (i != n)
+        {
+            for (j=n-1;j<=0;j--)
+            {
+                echange(T,i,j);
+                for (k=0;k<n;k++)
+                {
+                    empile(p,T[k]);
+                }
+                echange(T,i,j);
+            }
+            i=i+1;
+        }
+        while (!est_vide(p))
+        {
+            for (k=n-1;k<=0;k--)
+                {
+                    depile(p,e);
+                    T[k]=*e;
+                }
+            for (j=0;j<n;j++)
+                {
+            printf("%d",T[j]);
+            }
+        printf("\n");
+        }
     }
-}*/
+
+}
 
 
 int main () {
     int T[] = {1,2,3};
     truc_rec (1,3,T);
+    truc_iter(1,3,T);
     return 0;
 }
