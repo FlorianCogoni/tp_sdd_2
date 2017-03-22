1bac67181af2918d40bcda00e5898d63e3c58bc5
olibouv <oli.bouv@yahoo.fr>
2017-03-22 21:45:03 +0100

    Create fonctionsTab.h

---
+++ fonctionsTab.h
@@ -0,0 +1,10 @@
+#ifndef FONCTIONS_TAB
+#define FONCTIONS_TAB
+
+void echange(int T[], int i, int j);
+void afficherTab(int T[], int n);
+int empileTab(pile_t * pPile, int T[], int n);
+int depileTab(pile_t * pPile, int T[], int n);
+
+
+#endif
