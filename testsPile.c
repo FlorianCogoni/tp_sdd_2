1bfb30c6ecf1a5eac93e0c5f5ed368e05a4a3d73
olibouv <oli.bouv@yahoo.fr>
2017-03-24 00:25:03 +0100

    check

--- testsPile.c
+++ testsPile.c
@@ -1,6 +1,42 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |   testsPile.c     |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+
+	Ce fichier contient la fonction de test des fonctions de pile.
+
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "pile.h"
+
+/*---------------------------------------------------------------------
+ 
+ Cette fonction teste :
+		-initPile
+		-empile 
+			* empilage normale (pile non pleine)
+			* empilage impossible (pile pleine)
+		-depile
+			* dépilage normale (pile non vide)
+			* dépilage impossible (pile vide)
+		-estVide
+			* sur des piles vides
+			* sur des piles non vides
+		-libererPile (pour cela on rempilera sur une pile vide, puis on libèrera)
+		
+
+*/
+
+
 
 void testsPile()
 {
@@ -10,7 +46,9 @@ void testsPile()
 	printf("\nTEST INITIALISATION\n");
 	printf("----------------------------------------------------\n");
 	
+	
 	printf("\n * pile initialisée de taille 5, vide :\n");
+	printf("\n(une taille négative provoque un core dumped si on l'utilise à l'empilage par exemple)\n");
 	pPile = initPile(5);
 	afficherPile(pPile);
 	
@@ -67,7 +105,7 @@ void testsPile()
 	printf("\ncode = %d\nélément dépilé : %d\n",code,elt);
 	afficherPile(pPile);
 	
-	printf("\n * puis on le dernier élément :\n\n");
+	printf("\n * puis dépile on le dernier élément :\n\n");
 	code = depile(pPile,&elt);
 	printf("code = %d\nélément dépilé : %d\n",code,elt);
 	afficherPile(pPile);
@@ -84,13 +122,20 @@ void testsPile()
 	
 	
 	
-	printf("\nTESTS LIBERATION\n");
+	printf("\nTEST LIBERATION\n");
 	printf("----------------------------------------------------\n");
-	
-	printf("\n * si on libère la pile :\n");
-	libererPile(pPile);
+	printf("\n * on rempile 2,3,4,5 :\n");
+	code = empile(pPile, 2);
+	printf("\ncode = %d\n", code);
+	code = empile(pPile, 3);
+	printf("\ncode = %d\n", code);
+	code = empile(pPile, 4);
+	printf("\ncode = %d\n", code);
+	code = empile(pPile, 5);
+	printf("\ncode = %d\n", code);
 	afficherPile(pPile);
-	empile(pPile,1);
+	printf("\n * et on libère la pile :\n");
+	libererPile(pPile);
 	afficherPile(pPile);
 	printf("la taille max n'est plus 5 et la pile est bien vide\n\n");
 }
