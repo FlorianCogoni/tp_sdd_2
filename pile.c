ccf329e05edb9ddc196845b2b0451226180fa10d
olibouv <oli.bouv@yahoo.fr>
2017-03-23 21:17:23 +0100

    Update pile.c

--- pile.c
+++ pile.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include "pile.h"
 
-pile_t * initPile(int tmax) {
-    pile_t * pPile = NULL;
-    pPile = (pile_t *) malloc(sizeof(pile_t)); /* est-ce que c'est pas un des trucs qu'il a reproché ? */
-    if (pPile != NULL) {
+pile_t * initPile(int tmax) 
+{
+    pile_t * pPile = NULL;						
+    pPile = (pile_t *) malloc(sizeof(pile_t));
+    if (pPile != NULL) 
+    {
         pPile->tmax = tmax;
         pPile->taille = 0;
         pPile->liste = (int *) malloc(tmax * sizeof(int));
@@ -13,45 +15,67 @@ pile_t * initPile(int tmax) {
     return pPile;
 }
 
-int estVide(pile_t * pPile){ /* J'ai changé le nom par cohérence */
+
+void libererPile(pile_t * pPile)
+{
+	free(pPile->liste);
+	free(pPile);
+}
+
+
+int estVide(pile_t * pPile)
+{
     int bool = 0;
-    if (pPile->taille == 0){
+    if (pPile->taille == 0)
+    {
         bool = 1;
     }
     return bool;
 }
 
-int empile(pile_t * pPile, int elt){
-    int code = 1;
-    if (pPile->taille == pPile->tmax){
-        code = 0;
+int empile(pile_t * pPile, int elt)
+{
+    int code = 0;
+    if (pPile->taille == pPile->tmax)
+    {
+        printf("\npile pleine\n");
     }
-    else{
+    else
+    {
         pPile->liste[pPile->taille] = elt;
         pPile->taille += 1;
+        code = 1;
     }
     return code;
 }
 
-int * depile(pile_t * pPile){
-    int * adresseElt = NULL;
-    if (pPile->taille > 0)
+int depile(pile_t * pPile, int * elt)
+{
+    int code = 0;
+    if (!estVide(pPile))
     {
-        adresseElt =  &(pPile->liste[pPile->taille]);
+        *elt = pPile->liste[pPile->taille - 1];
         pPile->taille -= 1;
+        code = 1;
     }
-    return adresseElt;
+    else
+    {
+        printf("\npile vide\n");
+    }
+    return code;
 }
 
 void afficherPile(pile_t * pPile)
 {
     int i;
     int * cour = pPile->liste;
+    printf("\ntaille max de la pile : %d\n",pPile->tmax);
+    printf("taille de la pile     : %d\n",pPile->taille);
+    printf("pile                  : ");
     for (i=0;i<pPile->taille;i++)
     {
         printf("%d ", (*cour));
         cour ++;
     }
-    printf("\n");
+    printf("<- sommet de la pile\n\n");
 }
-
