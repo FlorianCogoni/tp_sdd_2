4d154a85fb8e0cf7040237799491367eaf638b71
olibouv <oli.bouv@yahoo.fr>
2017-03-24 00:50:27 +0100

    check

--- testsTruc.c
+++ testsTruc.c
@@ -1,19 +1,38 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |   testsTruc.c     |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+
+	Ce fichier contient la fonction compare, qui est une fontion test pour truc.
+
+*/
+
 #include <stdio.h>
 #include "truc.h"
 #include "pile.h"
 #include "fonctionsTab.h"
 
+
+/*---------------------------------------------------------------------
+ 
+ Cette procédure compare l'exécution de truc_rec avec celle de truc_iter
+
+*/
+
 void compare(int i, int n, int T[])
 {
-    printf("Tableau initial : ");
+    printf("\nTableau initial : ");
     afficherTab(T,n);
-    printf("i : %d n : %d \n",i,n);
-    printf("-------------------------------- \n");
+    printf("i : %d, n : %d \n",i,n);
+    printf("---------------------\n");
     printf("Truc recursif : \n");
     truc_rec(i,n,T);
-    printf("-------------------------------- \n");
+    printf("----------------------\n");
     printf("Truc iteratif : \n");
     truc_iter(i,n,T);
 }
-
-
