d3f260b42717dc4b71ba94b0aea33e6440698e57
olibouv <oli.bouv@yahoo.fr>
2017-03-24 00:53:39 +0100

    check

--- testsTruc.h
+++ testsTruc.h
@@ -1,10 +1,23 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |   testsTruc.h     |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+
+	Ce fichier contient le prototype de la fonction compare, ainsi que les tableaux de tests.
+*/
+
 #ifndef TESTS_TRUC
 #define TESTS_TRUC
 
 void compare(int i, int n, int T[]);
 
-int T1[3] = {1,2,3};
-int T2[1] = {1};
-int T3[6] = {1,2,3,4,5,6};
+int T1[3] = {1,2,3};		/* tableau de base */
+int T2[1] = {1};			/* tableau à 1 élément, le tableau de 0 élément est interdit par la norme Ansi */
+int T3[5] = {1,2,3,4,5};	/* tableau de longueur 5, un tableau plus grand serait possible mais deviendrait illisible pour la comparaison */
 
 #endif
