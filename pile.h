9e56979f640971feee081cf8b8cec7129cd4e87d
olibouv <oli.bouv@yahoo.fr>
2017-03-23 21:17:50 +0100

    Update pile.h

--- pile.h
+++ pile.h
@@ -1,16 +1,18 @@
 #ifndef PILE
 #define PILE
 
-typedef struct pile {
-    int tmax;
-    int taille;
+typedef struct pile
+{
+    int   tmax;
+    int   taille;
     int * liste;
 } pile_t;
 
 pile_t * initPile(int tmax);
+void libererPile(pile_t * pPile);
 int estVide(pile_t * pPile);
 int empile(pile_t * pPile, int elt);
-int * depile(pile_t * pPile);
+int depile(pile_t * pPile, int * elt);
 void afficherPile(pile_t * pPile);
 
 #endif
