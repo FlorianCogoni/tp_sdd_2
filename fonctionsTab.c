0c586be2b0e007d077b7ddf5787afd54032990a2
olibouv <oli.bouv@yahoo.fr>
2017-03-23 23:39:34 +0100

    Update fonctionsTab.c

--- fonctionsTab.c
+++ fonctionsTab.c
@@ -1,50 +1,51 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |  fonctionsTab.c   |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+
+	Ce fichier contient la fonction d'échange d'éléments d'un tableau, et d'affichage de 
+	tableau.
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "pile.h"
 #include "fonctionsTab.h"
 
 
-void echange(int T[], int i, int j){
-    int temp = T[i-1];
+
+/*---------------------------------------------------------------------
+ 
+ Cette procédure échange 2 éléments d'un tableau T par leurs indices i 
+ et j. On considère ici que les indices de T commencent à 1.
+
+*/
+
+void echange(int T[], int i, int j)
+{
+    int temp = T[i-1];		/* on utilise un entier temporaire pour stocker une des 2 valeurs avant de la changer */
     T[i-1] = T[j-1];
     T[j-1] = temp;
 }
+
+/*---------------------------------------------------------------------
+ 
+ Cette procédure affiche un tableau T, n étant la longueur du tableau.
+
+*/
 
 void afficherTab(int T[], int n)
 {
     int j;
     for (j=0; j<n; j++)
-        {
+    {
         printf("%d ", T[j]);
-        }
+    }
     printf("\n");
 }
-
-/*int empileTab(pile_t * pPile, int T[], int n)
-{
-    int i, code = 0;
-    for (i=0;i<n;i++)
-    {
-        code = empile(pPile,T[i]);
-    }
-    return code;
-}
-
-int depileTab(pile_t * pPile, int T[], int n)
-{
-    int i, code = 1;
-    int * cour;
-    for (i=0;i<n;i++)
-    {
-        cour = depile(pPile);
-        if (cour == NULL)
-        {
-            code = 0;
-        }
-        else
-        {
-            T[i] = *cour;
-        }
-    }
-    return code;
-}*/
